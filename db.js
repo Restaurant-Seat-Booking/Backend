5870c389145cae66b0c88f8e3cd99d9284203939
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-14 00:29:53 +0530

    s

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-const mysql  = require("mysql")
+const mysql = require('mysql');
 
-const pool = mysql.createConnection({
-    host : "sql12.freesqldatabase.com",
+const con = mysql.createConnection({
+    host: "sql12.freesqldatabase.com",
     user: "sql12625954",
-    password : "lNZwT2ExDA",
-    database : "sql12625954",
-})
+    password: "lNZwT2ExDA",
+    database: "sql12625954"
+  });
 
-pool.connect((err)=>{
-    if(err) console.log(err);
-    else console.log("connected");
-})
+  con.connect(function(err) {
+    if (err) console.log(err) ;
+    else console.log("Connected!");
+
+  });
