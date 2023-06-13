e55ebd9bcc4a97ec99362b70668b8618e314d3ef
Ved Prakash <vedprakash182001@gmail.com>
2023-06-14 00:29:18 +0530

    server

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
-const mysql  = require("mysql")
+const mysql = require("mysql");
 
-const pool = mysql.createConnection({
-    host : "sql12.freesqldatabase.com",
-    user: "sql12625954",
-    password : "lNZwT2ExDA",
-    database : "sql12625954",
-})
+const pool = mysql.createPool({
+  connectionLimit: 10, // Maximum number of connections in the pool
+  host: "sql12.freesqldatabase.com",
+  user: "sql12625954",
+  password: "lNZwT2ExDA",
+  database: "sql12625954",
+});
 
-pool.connect((err)=>{
-    if(err) console.log(err);
-    else console.log("connected");
-})
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  
+  console.log("Connected to the database");
+
+  // Perform database operations here
+
+  connection.release(); // Release the connection back to the pool
+});
