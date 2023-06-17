4df34f04076e540f118599b181ee39f1764b3f70
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-17 14:39:54 +0530

    s

--- Controllers/Restaurant/reviews.js
+++ Controllers/Restaurant/reviews.js
@@ -34,6 +34,75 @@ const getreview = async (req, res) => {
     message: "server error"
   })
 }
-}
+};
 
-module.exports = getreview;
+const getuser = async (req, res) => {
+    try {
+      const email = req.body.email;
+    //   const restaurantid = req.body.restaurantid;
+      // console.log(email)
+      DB.query(`SELECT * FROM users WHERE user_email = '${email}'`, (error, result) => {
+        if (error) {
+          res.status(400).json({
+            error: true,
+            message: "database error",
+          });
+        } else {
+          if (result.length > 0) {
+            const user = result[0];// Access the first item in the result array
+            res.status(200).json({
+              error: false,
+              data: user,
+            });
+          } else {
+            res.status(200).json({
+              error: false,
+              data: null, // Return null if no restaurant found
+            });
+          }
+        }
+      });
+  
+  } catch (error) {
+    res.status(404).json({
+      error: true,
+      message: "server error"
+    })
+  }
+  };
+
+
+const getreviewpost = async (req, res) => {
+    try {
+      const restaurantid = req.body.restaurantid;
+      const review = JSON.stringify(req.body.review);
+    //   console.log(restaurantid);
+    //   console.log(review);
+  
+      DB.query(
+        `UPDATE reviews
+        SET review = '${review}'
+        WHERE restaurant_id = '${restaurantid}';`,
+        (error, result) => {
+          if (error) {
+            res.status(400).json({
+              error: true,
+              message: "database error",
+            });
+          } else {
+            res.status(200).json({
+              error: false,
+              message: "updated successfully",
+            });
+          }
+        }
+      );
+    } catch (error) {
+      res.status(404).json({
+        error: true,
+        message: "server error",
+      });
+    }
+  };
+
+module.exports = {getreview,getuser,getreviewpost};
--- Routes/review.js
+++ Routes/review.js
@@ -2,9 +2,11 @@ const express = require("express");
 const reviewRoutes = express.Router();
 
 
-const getreview = require("../Controllers/Restaurant/reviews");
+const {getreview,getuser,getreviewpost} = require("../Controllers/Restaurant/reviews");
 
 reviewRoutes.post("/review",getreview);
+reviewRoutes.post("/user",getuser);
+reviewRoutes.post("/reviewpost",getreviewpost);
 
 
 module.exports = reviewRoutes;
