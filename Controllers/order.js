402bf0efcd964b11e63cd6cd62fffb5017ebdf02
Ved Prakash <vedprakash182001@gmail.com>
2023-07-03 23:08:36 +0530

    final commit

--- Controllers/order.js
+++ Controllers/order.js
@@ -1,5 +1,5 @@
 const DB = require("../db");
-
+const { instance } = require("../payment");
 const getorders = async(req,res)=>{
     try {
         const email = req.body.email;
@@ -28,12 +28,27 @@ const getorders = async(req,res)=>{
 
 const placeOrder = async(req, res)=>{
   try {
-    res.status(200).json({
-      error : false
+    // console.log(req.body);
+    let amount = req.body.totalAmount;
+    amount  = amount*100;
+    var options = {
+      amount: amount,  // amount in the smallest currency unit
+      currency: "INR",
+      receipt: "order_rcptid_11"
+    };
+    instance.orders.create(options, function(err, order) {
+      if(err){
+          return res.status(500).json({
+            err
+          });
+        }
+          return res.status(200).json({order});
     })
+
   } catch (error) {
-    res.status(200).json({
-      error : true
+   return res.status(200).json({
+      error : true,
+      message : "server error"
     })
   }
 }
